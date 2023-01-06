48725bb9094d162989aeb70ca3efaeb0e8c97823
Christina Thrainer <christina.thrainer@student.tugraz.at>
2023-01-06 08:52:16 +0100

    preprocessing of tweets

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,4 @@
+from cmath import nan
 import pandas as pd
 import numpy as np
 
@@ -24,6 +25,8 @@ from wordcloud import WordCloud
 import re
 import string
 import ast
+import datasets
+from tqdm.auto import tqdm
 #spelling correction library
 import itertools
 from autocorrect import Speller
@@ -31,10 +34,15 @@ from autocorrect import Speller
 import spacy
 from spacy.language import Language
 from spacy_langdetect import LanguageDetector
+#
 # factory decorator for spacy language detetion below
 @Language.factory("language_detector") # uncomment for the first run
 def get_lang_detector(nlp, name):
   return LanguageDetector()
+
+#import en_core_web_sm
+
+#nlp = en_core_web_sm.load()
 nlp=spacy.load("en_core_web_sm") #"en_core_web_sm"
 nlp.add_pipe('language_detector', last=True)
 #function to remove emoji
@@ -115,16 +123,55 @@ def preprocessing(video_corpus, stem_or_lemma = 'lemma'):
   print(commentlen) #
   return pd.DataFrame(cleaned_text)
 #>-------------------------------------------
+def preprocessing_training(data):
+  progress_bar = tqdm(range(len(data)))
+  for idx in range(len(data)):
+    progress_bar.update(1)
+    result = data["text"][idx]
+    new_comment = ""
+    if(pd.isna(result)):
+      continue
+    #removing punctuation
+    result = result.translate(str.maketrans('','', string.punctuation)) 
+    #removing URLs
+    result = re.sub(r'http\S+', '', result)
+    #removing emails
+    result = re.sub(r'\S*@\S*\s?', '', result)
+    #removing emojis
+    result = remove_emojis(result)
+    #spelling correction
+    spell = Speller(lang='en')
+    result = spell(result)
+    #tokenize
+    result = nltk.tokenize.word_tokenize(result)
+    for idx2 in range(len(result)):
+      word = result[idx2]
+       #removing stop words
+      word = word.lower()
+      if(word not in nltk.corpus.stopwords.words('english')):
+        if(idx2 != 0):
+          new_comment += " "
+        new_comment += word
+    data["text"][idx] = new_comment
+
+#>-------------------------------------------
 def main():
   path = 'Comments.csv'
   data = readdata(path)
+  print(type(data))
   #print(data['comments'][0])
 #<-------------------------------------------  
   print(len(data))
   Comments_prep = preprocessing(data['comments'],"lemma")
   #print(Comments_prep)
   Comments_prep.to_csv('Comments_prep.csv', encoding='utf-8')
-#-------------------------------------------
+
+  tweets = readdata('Tweets.csv')
+  tweets = tweets.drop(columns=['textID', 'selected_text'])
+  tweets = tweets.rename(columns={"sentiment": "labels"})
+  tweets = preprocessing_training(tweets)
+  tweets.to_csv('Tweets_prep.csv', encoding='utf-8')
+#<-------------------------------------------
 
 if __name__ == "__main__":
     main()
